d6185eed004c2d4d147d41b40316daf128d34d62
zeekygeek <rajbck7@gmail.com>
2024-04-10 12:31:58 +0530

    lecture 01

--- Lecture 01 basics/01basics.js
+++ Lecture 01 basics/01basics.js
@@ -1,1 +1,18 @@
-console.log("Hello World")
+// console.log("Hello World");
+
+// console.table(["Hello","Hi","bye bye","howdy","people"]) //prints in tabular form
+
+// let a = 5;
+// let b = 10;
+
+// // a = 15;
+// let c = (a+b);
+// console.log(c);
+
+let a = 15;
+let b = "hello";
+let c = true;
+let d = null;
+let e;
+console.log(typeof a, typeof b, typeof c, typeof d, typeof e);
+console.log(typeof (a,b,c,d,e));
