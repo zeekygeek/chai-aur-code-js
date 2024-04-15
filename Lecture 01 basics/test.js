259a5e18c85d7943e58f1dbc5aaab51f0d9373fa
zeekygeek <rajbck7@gmail.com>
2024-04-15 17:45:57 +0530

    initial commit

---
+++ Lecture 01 basics/test.js
@@ -0,0 +1,30 @@
+let firstName = "hello"
+
+let lastName = "world"
+
+// console.log(firstName+" "+lastName);
+
+// console.log(`my name is ${firstName} and my surname is ${lastName}`);
+
+// console.log(`my name is ${firstName.toUpperCase()} and my surname is ${lastName}`);  
+
+let testName = "AbhishekBachhan";
+
+let slicedName = testName.slice(2,4);
+
+
+// console.log(slicedName);
+
+
+let userDetails = {
+    name: "ajay",
+    city: "darbhanga",
+    age: 28,
+    gender: "male",
+}
+
+console.log(userDetails.age)
+
+userDetails.name = "ashish"
+
+console.log(userDetails.name)
