0ef02c36cc3bb57ce378b1ba2112ee3e135f2400
zeekygeek <rajbck7@gmail.com>
2024-05-30 19:59:41 +0530

    initial commit

---
+++ Lecture 03 basics/001_functions.js
@@ -0,0 +1,45 @@
+// function calculateCartPrice(num1){
+//     return num1;
+// }
+// console.log(calculateCartPrice(2));
+
+
+
+// function calculateCartPrice(val1, val2, ...num1){
+//     return num1;  
+// }
+// console.log(calculateCartPrice(200, 400, 500, 600));
+
+
+// ----------------------------------------------------------------------------------------------------
+
+
+// const user = {
+//     username : "abhijit",
+//     price : 199
+// }
+
+// ------Passing OBJECT in FUNCTION-------------------------------------------------------------------
+// function handleObject(anyobject) {
+//     console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
+// }
+// handleObject(user);
+
+
+// -----Another Method of Passing OBJECT in FUNCTION--------------------------------------------------
+
+// handleObject({
+//     username : "sam",
+//     price : 19
+// })
+
+
+// -----Passing ARRAY in FUNCTION--------------------------------------------------------------------
+
+// const myNewArray = [200, 400, 600, 800]
+// function resultNewArray(getArray){
+//     return getArray[2];
+// }
+// console.log(resultNewArray(myNewArray));
+
+
