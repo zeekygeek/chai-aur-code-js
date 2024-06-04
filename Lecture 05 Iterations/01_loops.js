63f1e1e7c2bf69a487e4179a8106c68d2f0f9f1e
zeekygeek <rajbck7@gmail.com>
2024-06-04 20:31:48 +0530

    Initial Commit

---
+++ Lecture 05 Iterations/01_loops.js
@@ -0,0 +1,24 @@
+// for
+
+for (let i = 0; i < 10; i++) {
+    // console.log(i);
+}
+
+// ---------------------------------------------------
+
+
+
+// Nested Loop
+for (let i = 0; i <= 30; i++) {
+    // console.log(`Outer loop : ${i}`)
+    for (let j = 0; j <= 10; j++) {
+        // console.log(`Inner loop value : ${j} and inner loop ${i}`); 
+        // console.log(i , '*' , j , '=' , i*j)
+        // console.log(`${i} * ${j} = ${i*j}`)   
+    }
+}
+
+
+// ---------------------------------------------------
+
+
