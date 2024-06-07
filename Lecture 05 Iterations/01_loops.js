6b0fe6f6d9ba94ba1dfc037f8ad795b49ee4f026
zeekygeek <rajbck7@gmail.com>
2024-06-07 21:10:23 +0530

    another commit

--- Lecture 05 Iterations/01_loops.js
+++ Lecture 05 Iterations/01_loops.js
@@ -1,15 +1,15 @@
-// for
+// for loop ::
 
 for (let i = 0; i < 10; i++) {
     // console.log(i);
 }
 
-// ---------------------------------------------------
+// -----------------------------------------------------------------------
 
 
 
 // Nested Loop
-for (let i = 0; i <= 30; i++) {
+for (let i = 0; i <= 10; i++) {
     // console.log(`Outer loop : ${i}`)
     for (let j = 0; j <= 10; j++) {
         // console.log(`Inner loop value : ${j} and inner loop ${i}`); 
@@ -19,6 +19,30 @@ for (let i = 0; i <= 30; i++) {
 }
 
 
-// ---------------------------------------------------
+//------------------------------------------------------------------------
+
+// BAISC SYNTAX OF loop
+
+let myArray = ['hindi', 'english', 'maths', 'physics']
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    console.log(element);
+}
+
+//------------------------------------------------------------------------
 
 
+// BREAK & CONTINUE
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log("")
+//     if (i === 5){
+//         console.log("Breaking the loop");
+//         break;
+//     }   
+//     for (let j = 1; j <= 10; j++) {
+//         console.log(`${i} * ${j} = ${i*j}`);   
+//     }  
+// }
+
+//-------------------------------------------------------------------------
