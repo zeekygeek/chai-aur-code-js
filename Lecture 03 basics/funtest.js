00d93954e7f5d47c75c7993ef85197f6bf1e831a
zeekygeek <rajbck7@gmail.com>
2024-06-17 17:51:24 +0530

    initial commit

--- Lecture 03 basics/funtest.js
+++ Lecture 03 basics/funtest.js
@@ -9,3 +9,30 @@
 // finalname = resultName(tryObj)
 
 // console.log(finalname)
+
+// let month = 'Jan'
+// switch(month){
+//     case 'Jan' :
+//         console.log('this month is January')
+//     break;
+//     case 'Feb' :
+//         console.log('this month is February')
+//     break;
+//     case 'Mar' :
+//         console.log('this month is March')
+//     break;
+//     default :
+//     console.log('no month provided')
+// }
+
+
+// NUMBER SQUARE TABLE 
+for (let i = 0; i <= 100; i++) {
+    for (let j = 0; j <= 100; j++) {
+        if(i != j){
+            continue
+        }
+        console.log(`${i} * ${j} = ${i*j}`)
+        // console.log(`${i} ^ 2 = ${i*j}`)
+    }
+}
