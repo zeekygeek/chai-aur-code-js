f23c5e8eb2ca559a4058340afbc0ff342e279a3b
zeekygeek <rajbck7@gmail.com>
2024-07-05 23:30:36 +0530

    initial commit

--- Lecture 05 Iterations/st.js
+++ Lecture 05 Iterations/st.js
@@ -1,11 +1,21 @@
-const tryObj = {
-    js: "javascript",
-    py: "python",
-    cpp: "c++",
-    rb: "ruby",
-  };
+// const tryObj = {
+//     js: "javascript",
+//     py: "python",
+//     cpp: "c++",
+//     rb: "ruby",
+//   };
 
   
-console.log(tryObj['js'])
+// console.log(tryObj['js'])
 
-console.log(Object.keys(tryObj))
+// console.log(Object.keys(tryObj))
+
+
+function changecol(e){
+    document.getElementById('title').style.color = e.target.value;
+}
+
+let box = document.querySelectorAll('.box')
+    box.addEventListener('click', function changecol(e) {
+        document.getElementById('title').style.color = e.target.style.backgroundColor;
+    })
